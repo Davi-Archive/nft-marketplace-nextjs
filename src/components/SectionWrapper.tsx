cb6ca55b8d46d97c0e6326b6f3769601750c8cb9
davi38 <davi4alves@gmail.com>
2022-10-12 22:49:14 -0300

    feat: decentralized hosting

--- src/components/SectionWrapper.tsx
+++ src/components/SectionWrapper.tsx
@@ -20,53 +20,53 @@ const SectionWrapper = ({
   reverse
 }: SecWrapperData) => {
   return (
-      <section
-        className={`min-h-screen ${styles.section}
+    <section
+      className={`min-h-screen ${styles.section}
       ${reverse ? styles.bgWhite : styles.bgPrimary}
       ${banner}`}
-      >
-        <div
-          className={`flex items-center
+    >
+      <div
+        className={`flex items-center
         ${reverse ? styles.boxReverseClass : styles.boxClass}
         w-11/12 sm:w-full minmd:w-3/4`}
-        >
-          <div
-            className={`${styles.descDiv}
+      >
+        <div
+          className={`${styles.descDiv}
           ${reverse ? " fadeRightMini" : " fadeLeftMini"}
           ${reverse ? styles.textRight : styles.textLeft}
         `}
-          >
-            <h1
-              className={`
+        >
+          <h1
+            className={`
           ${reverse ? styles.blackText : styles.whiteText}
           ${styles.h1Text}`}
-            >
-              {title}
-            </h1>
-            <p
-              className={`
+          >
+            {title}
+          </h1>
+          <p
+            className={`
           ${reverse ? styles.blackText : styles.whiteText}
           ${styles.descriptionText}`}
-            >
-              {description}
-            </p>
-            {showBtn && (
-              <Button
-                assetUrl={assets.expo}
-                link="https://expo.dev/@Davi38/react_native_app?serviceType=classic&distribution=expo-go"
-              />
-            )}
-          </div>
-          <div className={`flex-1 ${styles.flexCenter}p-8 sm:px-0`}>
-            <img
-              src={mockupImg}
-              alt="mockup"
-              className={`
+          >
+            {description}
+          </p>
+          {showBtn && (
+            <Button
+              assetUrl={assets.expo}
+              link="https://expo.dev/@davi38/client?serviceType=classic&distribution=expo-go"
+            />
+          )}
+        </div>
+        <div className={`flex-1 ${styles.flexCenter}p-8 sm:px-0`}>
+          <img
+            src={mockupImg}
+            alt="mockup"
+            className={`
            ${reverse ? " fadeLeftMini" : " fadeRightMini"}
           ${styles.sectionImg}`}
-            />
-          </div>
+          />
         </div>
+      </div>
     </section>
   );
 };
